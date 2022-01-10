c87e712a07c81eba8fea91b802f57da490c85741
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 16:37:46 +0300

    feat(Structure): Implement the basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import './App.css';
+import { css, Global } from '@emotion/react';
 import { Router } from './router';
 
+let globalStyles = css`
+  :root {
+    --color-background: #f2f0f9;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: var(--color-background);
+    font-family: 'Inter', sans-serif;
+  }
+`;
+
 export function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <Global styles={globalStyles} />
+    </>
+  );
 }
