47927eb5356a7f85067ceee0b8384c4d426148bc
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 17:04:02 +0300

    feat(Container): Implement a container

---
+++ src/pages/index.styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  min-height: 50vh;
+  margin: 0 auto;
+  padding: 50px 100px 100px;
+`;
