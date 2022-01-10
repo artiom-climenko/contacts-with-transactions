8d1374ae16b6c0ea97c23998c43b45af7211db8b
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 18:00:44 +0300

    feat(Main Window): Implement the main window

---
+++ src/components/MainWindow/index.styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin-top: 16px;
+  background-color: var(--color-background-primary);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+`;
+
+export const FilterAndSearch = styled.div`
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+`;
---
+++ src/components/MainWindow/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, FilterAndSearch } from './index.styles';
+
+export function ContactsContainer() {
+  return (
+    <Container>
+      <FilterAndSearch>
+
+      </FilterAndSearch>
+    </Container>
+  );
+}
