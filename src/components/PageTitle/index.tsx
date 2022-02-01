eb61e6964547e2ebad6fb1739dab6fec198bdaa5
Artiom Climenko <artiom.climenko@yandex.ru>
2022-02-01 11:41:44 +0300

    feat(Dark Theme): Implementation of the dark theme

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { LanguagesChangeButton, Title, TitleWrapper } from './index.styles';
 import { useTranslation } from 'react-i18next';
+import { useTheme } from '../../providers';
 
 export function PageTitle() {
   const { t, i18n } = useTranslation();
-  const changeLanguage = (language: string) => {
-    i18n.changeLanguage(language);
-  };
+
+  let handleChangeLanguage = useCallback(
+    (language: string) => {
+      i18n.changeLanguage(language).then((t) => {
+        t('key');
+      });
+    },
+    [i18n],
+  );
+
+  let { toggleTheme } = useTheme();
 
   return (
     <TitleWrapper>
       <Title>{t('title')}</Title>
       <div>
-        <LanguagesChangeButton onClick={() => changeLanguage('en')}>
+        <LanguagesChangeButton onClick={() => handleChangeLanguage('en')}>
           EN
         </LanguagesChangeButton>
-        <LanguagesChangeButton onClick={() => changeLanguage('ru')}>
+        <LanguagesChangeButton onClick={() => handleChangeLanguage('ru')}>
           RU
+        </LanguagesChangeButton>
+        <LanguagesChangeButton onClick={() => toggleTheme()}>
+          {t('changeTheme')}
         </LanguagesChangeButton>
       </div>
     </TitleWrapper>
