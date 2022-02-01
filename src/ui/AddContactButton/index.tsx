17c073afd2e1a2f5a147c155b1f927d040984646
Artiom Climenko <artiom.climenko@yandex.ru>
2022-02-01 18:58:25 +0300

    feat(Create Contact): Implement create contact

--- src/ui/AddContactButton/index.tsx
+++ src/ui/AddContactButton/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from './index.styles';
 
-export function AddContactButton() {
+export interface IAddContactButtonProps {
+  setOpenCreateModal: (value: boolean) => void;
+}
+
+export function AddContactButton({
+  setOpenCreateModal,
+}: IAddContactButtonProps) {
   let { t } = useTranslation();
-  return <Button type="button">{t('createContact')}</Button>;
+  return (
+    <Button type="button" onClick={() => setOpenCreateModal(true)}>
+      {t('createContact')}
+    </Button>
+  );
 }
